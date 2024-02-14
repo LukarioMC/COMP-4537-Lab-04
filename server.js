2c6f2779f70e8b746950e47e8e8e64d22fc2cf8c
Dakaro Mueller <LukarioMC@gmail.com>
2024-02-14 12:04:36 -0800

    Simplifying server logic, refactoring

--- server.js
+++ server.js
@@ -4,36 +4,13 @@
  */
 const http = require('http');
 const url = require('url');
-const utils = require('../modules/utils');
+const utils = require('./modules/utils');
 const dict = require('./lang/en/en');
 const PORT = process.env.PORT || 8000;
 
-function handleDate(req, res) {
-    const parsedAddress = url.parse(req.url, true);
-    const name = parsedAddress.query['name'] || 'Guest';
-    const currDate = utils.getDate();
-    const response = dict.EN.dateString
-        .replace('{0}', name)
-        .replace('{1}', currDate);
-    const data = `<span style="color: blue">${response}</span>`;
-    respondHTML(res, 200, data);
-}
-
 const routes = {
-    '/COMP4537/labs/3/getDate': handleDate,
-    '/getDate': handleDate,
+    '/api/definitions': utils.handleDefiniton,
 };
-
-function respondHTML(res, statusCode, data) {
-    res.writeHead(statusCode, { 'content-type': 'text/html' });
-    res.write(data);
-    res.end();
-}
-
-function logRequest(req) {
-    console.log('The server received a request!');
-    console.log('Request details: ' + req.url);
-}
 
 function handleRequest(req, res) {
     const reject = () => respondHTML(res, 400, 'Invalid request');
@@ -44,18 +21,8 @@ function handleRequest(req, res) {
 }
 
 http.createServer((req, res) => {
-    logRequest(req);
+    utils.logRequest(req);
     handleRequest(req, res);
 }).listen(PORT);
 
 console.log('Server is running on port ' + PORT + ' for requests...');
-
-// let fs = require('fs');
-// fs.readFile('.' + q.pathname, (err, data) => {
-//     if (err) {
-//         // Handle 404 error
-//     }
-//     res.writeHead(200, { 'content-type': 'text/html' });
-//     res.write(data);
-//     return res.end();
-// });
