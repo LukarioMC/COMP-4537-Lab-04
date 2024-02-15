d1bd85a7e9414f2d16dd0ec57f44722da3a646d1
Dakaro Mueller <LukarioMC@gmail.com>
2024-02-14 23:18:37 -0800

    Redid definition implementation, Added documentation, Nearly completed!

--- server.js
+++ server.js
@@ -4,24 +4,34 @@
  */
 const http = require('http');
 const url = require('url');
-const utils = require('./modules/utils');
+const { respondJSON, logRequest } = require('./modules/utils');
+const { handleDefinition } = require('./modules/definitions');
 const PORT = process.env.PORT || 8000;
 
+// Maps the different possible routes to handlers
 const routes = {
-    '/api/definitions': utils.handleDefinition,
-    '/api/definitions/': utils.handleDefinition,
+    '/api/definitions': handleDefinition,
+    '/api/definitions/': handleDefinition,
 };
 
+/**
+ * Handles an incoming HTTP request. Delegates handling to routing map.
+ * Rejects invalid requests with 404 status code.
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
 function handleRequest(req, res) {
-    const reject = () => utils.respondHTML(res, 400, 'Invalid request');
+    const reject = () =>
+        respondJSON(res, 404, { status: 404, error: 'Invalid request' });
     if (!req || !req.url) reject();
     const parsedAddress = url.parse(req.url, true);
     const handler = routes[parsedAddress.pathname] || reject;
     if (handler) handler(req, res);
 }
 
+// Begins the server and attaches the request handler.
 http.createServer((req, res) => {
-    utils.logRequest(req);
+    logRequest(req);
     handleRequest(req, res);
 }).listen(PORT);
 
