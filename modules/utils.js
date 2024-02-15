f444ba8d33c12c031cb7d764242ca50af3f0fe9b
Dakaro Mueller <LukarioMC@gmail.com>
2024-02-14 23:17:24 -0800

    Refactored utils, added documentation

--- modules/utils.js
+++ modules/utils.js
@@ -1,26 +1,39 @@
-const { storeDefinition, retrieveDefinition } = require('definitions.js');
-
-exports.handleDefinition = (req, res) => {
-    const method = res.method;
-    if (method === 'GET') retrieveDefinition(req, res);
-    else if (method === 'POST') storeDefinition(req, res);
-};
-
+/**
+ * Responds to a request with HTML data. No parsing of the data is done.
+ * @param {http.ServerResponse} res
+ * @param {number} statusCode   HTML status code
+ * @param {object} data Raw HTML code
+ */
 function respondHTML(res, statusCode, data) {
     res.writeHead(statusCode, { 'content-type': 'text/html' });
     closeWithData(res, data);
 }
 
+/**
+ *
+ * @param {http.ServerResponse} res
+ * @param {number} statusCode   HTML status code
+ * @param {object} data JSON object to stringify
+ */
 function respondJSON(res, statusCode, data) {
     res.writeHead(statusCode, { 'content-type': 'text/json' });
-    closeWithData(res, data);
+    closeWithData(res, JSON.stringify(data));
 }
 
+/**
+ * Closes and ends a response with some data.
+ * @param {http.ServerResponse} res
+ * @param {object} data
+ */
 function closeWithData(res, data) {
     res.write(data);
     res.end();
 }
 
+/**
+ * Logs the incoming request to the console.
+ * @param {http.IncomingMessage} req
+ */
 function logRequest(req) {
     console.log('The server received a request!');
     console.log('Request details: ' + req.url);
